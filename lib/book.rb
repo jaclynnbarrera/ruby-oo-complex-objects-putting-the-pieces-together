afd4ba64ff8dd546b5070772c5f57c9d910a013c
Jaclyn <jaclynnbarrera@gmail.com>
2021-01-20 21:02:44 -0500

    commit tree 685745f9df16c32858b7177bf50b8c2350ad292c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
 
+class Book
+    #properties: title, author, page count, genre, ability to turn pages
+    def initialize(title)
+        @title = title
+    end
 
+    def title #getter
+        @title
+    end
+
+    def author=(author) #setter
+        @author = author
+    end
+
+    def author #getter
+        @author
+    end
+
+    def page_count=(num) #setter
+        @page_count = num
+    end
+
+    def page_count #getter
+        @page_count
+    end
+
+    def genre=(genre) #setter
+        @genre = genre
+    end
+
+    def genre #getter
+        @genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
